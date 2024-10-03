b18413cb79e4bd68eaa6e21dc5e1236f8d8b7dbd
Sergo_Kaz <wwe619dx@ukr.net>
2024-10-03 20:01:44 +0200

    finish commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const toggleSidebar = document.querySelector(".toggle-sidebar");
+const sidebar = document.querySelector(".sidebar");
+
+toggleSidebar.addEventListener("click", () => {
+  const windowInnerWidth = window.innerWidth;
+
+  sidebar.classList.toggle("hidden");
+
+  if (windowInnerWidth < 720) {
+    toggleSidebar.classList.add("toggle-sidebar-none");
+  }
+});
+
+const sidebarClose = document.querySelector(".sidebar-close");
+
+sidebarClose.addEventListener("click", () => {
+  const windowInnerWidth = window.innerWidth;
+
+  sidebar.classList.remove("hidden");
+
+  if (windowInnerWidth < 720) {
+    toggleSidebar.classList.remove("toggle-sidebar-none");
+  }
+});
